697aaab74d9efd13036e60ac9f891e86af5de862
Mario Burga Uriarte <cyberespia@gmail.com>
2019-02-27 22:16:37 -0500

    Initial Commit

---
+++ wservicestest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wservicestest
+//
+//  Created by Mario Burga on 2/27/19.
+//  Copyright © 2019 mihost.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
